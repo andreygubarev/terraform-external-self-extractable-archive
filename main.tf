bc066915f91ba4605aecfff8899e9df3040cea6c
Andrey Gubarev <mylokin@me.com>
2021-10-13 10:27:50 +0300

    init thingy

--- main.tf
+++ main.tf
@@ -1,1 +1,17 @@
+data "external" "this" {
+  program = ["sh", "${path.module}/external.sh"]
 
+  query = {
+    archive_dir    = var.archive_dir
+    file_name      = var.file_name
+    label          = var.label
+    startup_script = var.startup_script
+
+    gzip     = var.gzip
+    needroot = var.needroot
+  }
+}
+
+output "result" {
+  value = data.external.this.result
+}
--- variables.tf
+++ variables.tf
@@ -14,3 +14,30 @@ variable "workload" {
 locals {
   workload_prefix = "${var.workload}-"
 }
+
+################################################################################
+# Workload
+################################################################################
+variable "archive_dir" {
+  type = string
+}
+
+variable "file_name" {
+  type = string
+}
+
+variable "label" {
+  type = string
+}
+
+variable "startup_script" {
+  type = string
+}
+
+variable "gzip" {
+  type = bool
+}
+
+variable "needroot" {
+  type = bool
+}
