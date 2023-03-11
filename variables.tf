ab0829d5fdc9411dd7dfabbedb0651a3f2132ad9
Andrey Gubarev <mylokin@me.com>
2023-03-12 01:18:27 +0400

    ensure entrypoint script is executable

--- variables.tf
+++ variables.tf
@@ -1,6 +1,7 @@
 variable "label" {
   type = string
 
+  description = "The label that describes the archive."
   validation {
     condition     = can(regex("^[-a-z]+$", var.label))
     error_message = "Invalid value: label."
@@ -9,17 +10,26 @@ variable "label" {
 
 variable "archive_dir" {
   type = string
-}
 
-variable "startup_script" {
-  type = string
+  description = "The directory that contains the files to be archived."
 }
 
 variable "file_name" {
   type = string
+
+  description = "The name of the archive file."
 }
 
 variable "needroot" {
   type    = bool
   default = false
+
+  description = "Whether the script needs to be run as root."
+}
+
+variable "startup_script" {
+  type = string
+  default = "./entrypoint.sh"
+
+  description = "The script to run when the archive is extracted."
 }
